c1e9a52260658ad33cb66d8807bb0f42fb00d708
Diwanshu Gangwar <diwanshugang123@gmail.com>
2024-09-23 23:41:43 +0530

    first commit

---
+++ index.jsx
@@ -0,0 +1,79 @@
+
+import { useEffect, useState } from "react";
+import "./scroll.css";
+
+export default function ScrollIndicator({url}) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+
+  async function fetchData(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(getUrl);
+      const data = await response.json();
+
+      if (data && data.products && data.products.length > 0) {
+        setData(data.products);
+        setLoading(false);
+      }
+    } catch (e) {
+      console.log(e);
+      setErrorMessage(e.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchData(url);
+  }, [url]);
+
+  function handleScrollPercentage() {
+    const scrollTop =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    const scrollHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    const percentage = (scrollTop / scrollHeight) * 100;
+
+    setScrollPercentage(percentage);
+    console.log(percentage);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollPercentage);
+
+    return () => {
+      window.removeEventListener("scroll", handleScrollPercentage);
+    };
+  }, []);
+
+  console.log(data, scrollPercentage);
+
+  if (errorMessage) {
+    return <div>Error! {errorMessage}</div>;
+  }
+  if (loading) {
+    return <div>Loading Data! Please wait.. </div>;
+  }
+  return (
+    <div>
+      <div className="top-container">
+        <h1>Custom Scroll Indicator</h1>
+        <div className="scroll-progress-tracking">
+          <div
+            className="current-progress-bar"
+            style={{ width: `${scrollPercentage}%` }}
+          ></div>
+        </div>
+      </div>
+
+      <div className="data-container">
+        {data && data.length > 0
+          ? data.map((dataItem) => <p key={dataItem.id}>{dataItem.title}</p>)
+          : null}
+      </div>
+    </div>
+  );
+}
